a7d948d43eb83a71cfaccc50f115a2c36e04bc12
Yoshimivi <104239593+Yoshimivi@users.noreply.github.com>
2022-05-05 07:21:20 -0300

    Delete Curso em video/class04 directory

--- Curso em video/class04/ex001/script.js
+++
@@ -1,6 +0,0 @@
-
-        window.alert('Primeira msg')
-        //window.confirm('Está gostando de JS?')
-        window.prompt('Qual é seu nome?')
-        // deixar comentario dentro script
-        window.alert('segunda msg')
