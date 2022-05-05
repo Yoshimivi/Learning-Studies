2d68a12d57679b4fc42f1e3865227047bcc2b96c
Yoshimivi <104239593+Yoshimivi@users.noreply.github.com>
2022-05-05 07:23:36 -0300

    Classes and study exercises

---
+++ Curso em video/class04/ex001/script.js
@@ -0,0 +1,6 @@
+
+        window.alert('Primeira msg')
+        //window.confirm('Está gostando de JS?')
+        window.prompt('Qual é seu nome?')
+        // deixar comentario dentro script
+        window.alert('segunda msg')
