1d17e3fcc54805ed0c0d466526df1b4e762119fd
Yoshimivi <yoshimisuzaki@hotmail.com>
2022-05-26 10:25:49 -0300

    DIO - Exercises

---
+++ Bootcamp Spread - DIO/Modulo III/Modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs'
+
+console.log(mostraIdade('Victor', 26))
+console.log(mostraCidade('Victor', 'Guarulhos'))
+console.log(mostraHobby('Victor', 'Jogar'))
